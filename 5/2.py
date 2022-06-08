a225e84d2a190a1b158671171eab93ea57545911
Paul Keller <contact.keller.paul@gmail.com>
2022-06-08 20:19:41 +0200

    added comments and detected edges of region

--- 5/2.py
+++ 5/2.py
@@ -37,16 +37,24 @@ def findEdgePixel(img, x, y, threshold):
       return j, y
   return None, None
 
+# start X index to search for edge
 EDGE_FIND_X = 0
+# start Y index to search for edge
 EDGE_FIND_Y = 100
+# Threshold of value when a pixel is considered an edge
 EDGE_THRESHOLD = 200
+# the Input Picture path
 PICTURE_INPUT = "Rauschbild.png"
+# Filtering Anything below EDGE_LOW_FILTER and anything above EDGE_HIGH_FILTER
 EDGE_LOW_FILTER = 50
 EDGE_HIGH_FILTER = 255
+# if 8 neighbour is used
 USE_8_NEIGHBOURS = False
+# the edge detection filter method
 FILTER_FUNCTION = sobel
 INVERT_PICTURE= False
 
+# pixel class that keeps track of its visited neighbours
 class Pixel:
   def __init__(self, x, y):
     self.x = x
@@ -65,6 +73,7 @@ class Pixel:
     return hash((self.x, self.y))
   def __repr__(self) -> str:
     return f"({self.x}, {self.y})"
+  # returns the next neighbour in the priority order and removes it from "not visited" list
   def get_next_neighbour(self):
     if len(self.neighbours) > 0:
       pair = self.neighbours.pop(0)
@@ -72,34 +81,46 @@ class Pixel:
     else:
       return None
 
+# return true if the pixel is in the picture
 def is_in_bounds(img, pixel):
   if pixel.x < 0 or pixel.x >= img.shape[1] or pixel.y < 0 or pixel.y >= img.shape[0]:
     return False
   return True
 
 def paint_edges(img, x, y, threshold):
+  # create new image that is rgb and transform img to rgb
   painted_img = np.zeros((img.shape[0], img.shape[1], 3)).astype(int)
   for i in range(img.shape[0]):
     for j in range(img.shape[1]):
       painted_img[i,j] = np.array([img[i,j], img[i,j], img[i,j]])
-  
+  # set start pixel to red
   painted_img[y,x] = np.array([255,0,0])
+  # and add it to the edge list
   edges_visited = [Pixel(x,y)]
+  # get the next pixel to be visited
   nextPixel = edges_visited[-1].get_next_neighbour()
+  # used if we enter an infinit loop
   stepcounter = 0
+  # used if we enter a dead end
   lastLength = 0
   noChangesCounter = 0
+  # loop through next Pixel and its neighbours
   while True:
+    # increase this every loop
     stepcounter += 1
     if stepcounter % 1000 == 0:
+      # if we have a somewhat similar path length since the last 1000 checks, increase the noChangesCounter
       if abs(lastLength - len(edges_visited)) < 10:
         noChangesCounter+=1
       else:
         lastLength = len(edges_visited)
+      # if we have a similar path length after 10.000 visits, exit the loop
       if noChangesCounter > 10:
         print ("Breaking due to no progress")
         break
+      # output for debug
       print("Step:", stepcounter/1000000, "Path Length:", len(edges_visited))
+    # if we somehow reached an infinit loop, break the loop
     if stepcounter > 1000000:
       print("Error: infinite loop")
       break
@@ -128,22 +149,30 @@ def paint_edges(img, x, y, threshold):
           edges_visited.append(nextPixel)
     # get the next neighbour
     nextPixel = edges_visited[-1].get_next_neighbour()
+  # after finishing the loop, paint the edges
   for edge in edges_visited:
     # paint the neighbour
     painted_img[edge.y, edge.x] = np.array([255,0,0])
   return painted_img
 
 def main():
+  # Read in picture
   img = plt.imread(os.path.join(os.path.dirname(__file__), PICTURE_INPUT))
+  # if picture is rgb, convert to grayscale
   if img.ndim == 3:
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-  img = img*255
+  # if picture is in range between 0 and 1
+  if img.max() <= 1:
+    img = img*255
+  # use Edge detection filter and cap the output to 0 and EDGE_HIGH_FILTER
   edge_img = np.clip(FILTER_FUNCTION(img),0,EDGE_HIGH_FILTER)
+  # if the picture should be inverted, invert it
   if INVERT_PICTURE:
     edge_img = (~(np.array(edge_img, np.uint8)))
+  # otherwise use a threshold between EDGE_LOW_FILTER and EDGE_HIGH_FILTER to clean up the edge picture
   else:
     edge_img = cv.threshold(edge_img, EDGE_LOW_FILTER, EDGE_HIGH_FILTER, cv.THRESH_BINARY)[1].astype(int)
-  #edge_img = img.copy().astype(int)
+  # plot the original and edge picture
   plt.subplot(231)
   plt.imshow(img, cmap="gray")
   plt.title("Original")
@@ -151,9 +180,11 @@ def main():
   plt.imshow(edge_img, cmap="gray")
   plt.title(FILTER_FUNCTION.__name__)
   
-  
+  # start searching for edge pixel
   x,y = findEdgePixel(edge_img, EDGE_FIND_X, EDGE_FIND_Y, EDGE_THRESHOLD)
+  # if edge pixel was found
   if x is not None and y is not None:
+    # paint the edge pixels and show the result
     plt.subplot(233)
     painted_img = paint_edges(edge_img, x, y, EDGE_THRESHOLD)
     plt.imshow(painted_img)
--- 5/3.py
+++ 5/3.py
@@ -1,25 +1,29 @@
 import os
+from threading import Thread
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
 
-def kernel_transform(img, kernel):
-  return cv.filter2D(img, -1, kernel)
-
-def loG(img):
-  return kernel_transform(img, (1/16)*np.array([[0,1,2,1,0],[1,0,-2,0,1],[2,-2,-8,-2,2],[1,0,-2,0,1],[0,1,2,1,0]]))
-def doG(img):
-  return kernel_transform(img, np.array([[1,4,6,4,1],[4,0,-8,0,4],[6,-8,-28,-8,6],[4,0,-8,0,4],[1,4,6,4,1]]))
-def laplace(img):
-  return kernel_transform(img, np.array([[0,1,0],[1,-4,1],[0,1,0]]))
+# start X index to search for edge
+SEED_X = 70
+# start Y index to search for edge
+SEED_Y = 100
+# Threshold of value when a pixel is considered an edge
+THRESHOLD = 70
+# the Input Picture path
+PICTURE_INPUT = "Rauschbild.png"
 
 def paint_area(img, seed, threshold):
+  # create a copy of the image in rgb
   painted_img = np.zeros((img.shape[0], img.shape[1], 3)).astype(int)
   for i in range(img.shape[0]):
     for j in range(img.shape[1]):
       painted_img[i,j] = np.array([img[i,j], img[i,j], img[i,j]])
+  # used for saving the region
+  region = [seed]
+  # stack for visiting neighbours
   visited = [seed]
-
+  print("Finding region")
   while len(visited) > 0:
     x,y = visited.pop()
     painted_img[y,x] = np.array([255,0,0])
@@ -29,19 +33,42 @@ def paint_area(img, seed, threshold):
           if img[i,j] > threshold:
             if not np.array_equal(painted_img[i,j], np.array([255,0,0])):
               painted_img[i,j] = np.array([255,0,0])
+              if (j, i) not in  region:
+                  region.append((j, i))
               visited.append((j,i))
+  print("Finding region edges")
+  regionEdge = []
+  for r in region:
+    found_edge = False
+    for i in range(r[0]-1, r[0]+2):
+      for j in range(r[1]-1, r[1]+2):
+        if i == r[0] and j == r[1]:
+          continue
+        if (i,j) not in region:
+          found_edge = True
+          break
+      if found_edge:
+        break
+    if found_edge:
+      regionEdge.append(r)
+  print("Printing Edge Region")
+  for r in regionEdge:
+    painted_img[r[1], r[0]] = np.array([0,0,255])
   return painted_img
 
 
 def main():
-  img = plt.imread(os.path.join(os.path.dirname(__file__), 'Rauschbild_clean.png'))
-  img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-  img = img*255
+  # read in image
+  img = plt.imread(os.path.join(os.path.dirname(__file__), PICTURE_INPUT))
+  if img.ndim == 3:
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+  if img.max() <= 1:
+    img = img*255
   plt.subplot(221)
   plt.imshow(img, cmap="gray")
-  plt.title("Orig")
+  plt.title("Original")
   plt.subplot(222)
-  plt.imshow(paint_area(img, (75,100), 70).astype(int))
+  plt.imshow(paint_area(img, (SEED_X,SEED_Y), THRESHOLD).astype(int))
   plt.title("Painted")
   plt.show()
 
