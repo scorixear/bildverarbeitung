8725ec713eef2a081a69780ae95362613193d6d3
Paul Keller <contact.keller.paul@gmail.com>
2022-06-25 11:08:00 +0200

    added skeleton and finished 2

--- 7/2.py
+++ 7/2.py
@@ -11,7 +11,7 @@ def kernel(img, matrix):
       fits = True
       for x in range(matrix.shape[1]):
         for y in range(matrix.shape[0]):
-          if matrix[y,x] != 2:
+          if matrix[y,x] != None:
             if i+y-1 < 0 or i+y-1 >= img.shape[0] or j+x-1 < 0 or j+x-1 >= img.shape[1]:
               if matrix[y,x] != 0:
                 fits = False
@@ -23,49 +23,26 @@ def kernel(img, matrix):
           break
       if fits:
         newimg[i,j] = 1
-  return newimg
+  return newimg.astype('uint8')
 
-def dilute(img):
-  kernelImages = []
-  print("Kernel 1")
-  kernelImages.append(kernel(img, np.array([[0,2,1],[0,1,1],[0,2,1]])))
-  print("Kernel 2")
-  kernelImages.append(kernel(img, np.array([[0,0,2],[0,1,1],[2,1,1]])))
-  print("Kernel 3")
-  kernelImages.append(kernel(img, np.array([[0,0,0],[2,1,2],[1,1,1]])))
-  print("Kernel 4")
-  kernelImages.append(kernel(img, np.array([[2,0,0],[1,1,0],[1,1,2]])))
-  print("Kernel 5")
-  kernelImages.append(kernel(img, np.array([[1,2,0],[1,1,0],[1,2,0]])))
-  print("Kernel 6")
-  kernelImages.append(kernel(img, np.array([[1,1,2],[1,1,0],[2,0,0]])))
-  print("Kernel 7")
-  kernelImages.append(kernel(img, np.array([[1,1,1],[2,1,2],[0,0,0]])))
-  print("Kernel 8")
-  kernelImages.append(kernel(img, np.array([[2,1,1],[0,1,1],[0,0,2]])))
-  newimg = np.zeros(img.shape)
-  for i in range(img.shape[0]):
-    for j in range(img.shape[1]):
-      Value = 0
-      for k in kernelImages:
-        if k[i,j] == 1:
-          Value = 1
-          break
-      newimg[i,j]= Value
+def thin(img):
+  #kernel = np.array([[0,None,1],[0,1,1],[0,None,1]])
+  #hitandmis = cv.morphologyEx(img, cv.MORPH_HITMISS, kernel)
+  #newimg = cv.bitwise_and(img, cv.bitwise_not(hitandmis))
+  newimg = cv.bitwise_and(img, cv.bitwise_not(kernel(img, np.array([[0,None,1],[0,1,1],[0,None,1]]))))
+  newimg = cv.bitwise_and(newimg, cv.bitwise_not(kernel(img, np.array([[0,0,None],[0,1,1],[None,1,1]]))))
+  newimg = cv.bitwise_and(newimg, cv.bitwise_not(kernel(img, np.array([[0,0,0],[None,1,None],[1,1,1]]))))
+  newimg = cv.bitwise_and(newimg, cv.bitwise_not(kernel(img, np.array([[None,0,0],[1,1,0],[1,1,None]]))))
+  newimg = cv.bitwise_and(newimg, cv.bitwise_not(kernel(img, np.array([[1,None,0],[1,1,0],[1,None,0]]))))
+  newimg = cv.bitwise_and(newimg, cv.bitwise_not(kernel(img, np.array([[1,1,None],[1,1,0],[None,0,0]]))))
+  newimg = cv.bitwise_and(newimg, cv.bitwise_not(kernel(img, np.array([[1,1,1],[None,1,None],[0,0,0]]))))
+  newimg = cv.bitwise_and(newimg, cv.bitwise_not(kernel(img, np.array([[None,1,1],[0,1,1],[0,0,None]]))))
   return newimg
     
-  
 
-def checkDifference(img1, img2):
-  sum = 0
-  for i in range(img1.shape[0]):
-    for j in range(img1.shape[1]):
-      if img1[i,j] != img2[i,j]:
-        sum += 1
-  return sum
 
 def main():
-  img = plt.imread(os.path.join(os.path.dirname(__file__), 'cat2.jpg'))
+  img = plt.imread(os.path.join(os.path.dirname(__file__), 'skeleton.png'))
   if img.ndim == 3:
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
   if img.max() <= 1:
@@ -75,26 +52,30 @@ def main():
   oldPic = np.where(img > 127, 1, 0).astype('uint8')
   binary = np.where(img > 127, 1, 0).astype('uint8')
   
-  diluted = dilute(oldPic)
-  diff = checkDifference(oldPic, diluted)
-  while diff > 200:
-    plt.subplot(121)
-    plt.imshow(oldPic, cmap='gray')
-    plt.subplot(122)
-    plt.imshow(diluted, cmap='gray')
-    plt.show()
-    print("Recalculating dilute",diff)
-    oldPic = diluted
-    diluted = dilute(oldPic)
-    diff = checkDifference(oldPic, diluted)
+  thinned = thin(oldPic)
+  zeros = cv.countNonZero(thinned)
+  prevZeros = cv.countNonZero(oldPic)
+  while zeros != 0 and zeros != prevZeros:
+    #plt.subplot(121)
+    #plt.imshow(oldPic, cmap='gray')
+    #plt.axis('off')
+    #plt.subplot(122)
+    #plt.imshow(thinned, cmap='gray')
+    #plt.axis('off')
+    #plt.show()
+    print("Recalculating thinning",zeros)
+    oldPic = thinned
+    thinned = thin(oldPic)
+    prevZeros = zeros
+    zeros = cv.countNonZero(thinned)
     
     
   plt.subplot(121)
   plt.imshow(binary, cmap="gray")
   plt.title("Original")
   plt.subplot(122)
-  plt.imshow(diluted, cmap="gray")
-  plt.title("Diluted")
+  plt.imshow(oldPic, cmap="gray")
+  plt.title("Thinned")
   plt.show()
   
 
